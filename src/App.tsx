90596c91c0e75d59cc0cae4626cf332dcb1e9dfb
erntorii <erntorii@gmail.com>
2021-06-09 15:40:09 +0900

    add arrange components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import Sidebar from 'components/organisms/Sidebar';
+import Feed from 'components/organisms/Feed';
+import Widgets from 'components/organisms/Widgets';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      React App
+      <Sidebar />
+      <Feed />
+      <Widgets />
     </div>
   );
 }
